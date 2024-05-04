91273e7d4c80b466e0f024949cd8c29f004ed973
amit raj <amitraaaz0@gmial.com>
2024-05-04 20:00:49 +0530

    completed

---
+++ src/component/Footer.jsx
@@ -0,0 +1,32 @@
+ 
+ const Footer =()=>{
+    return(
+        <>
+           <div className=" flex  flex-wrap">
+                <div className=" m-5">
+                    <div className=" ">
+                        <img className="  w-80 m-3 h-36 items-center " src="https://img.freepik.com/premium-vector/open-book-with-launching-rocket-vector-linear-icon-missile-start-up-from-text-space-scientific-literature-library-reading-line-art-symbol-logo_570429-24747.jpg?w=360" />
+                        <p className=" text-center text-black font-mono ">2500</p>
+                    </div>
+                     <p className=" text-ml font-mono">enterprise organization sucessfully launched</p>
+                </div>
+                <div className=" m-5">
+                    <div className=" ">
+                        <img className="  w-80 m-3 h-36 items-center " src="https://upload.wikimedia.org/wikipedia/commons/9/93/Google_Contacts_icon.svg" />
+                        <p className=" text-center text-black font-mono ">2500</p>
+                    </div>
+                     <p className=" text-ml font-mono">enterprise organization sucessfully launched</p>
+                </div>
+                <div className=" m-5">
+                    <div className=" ">
+                        <img className="  w-80 m-3 h-36 items-center " src="https://play-lh.googleusercontent.com/9AZOTXU_CpreTFAXUPAmJNkm8VGCb1C90fjJ9pHGcVmpGMDSTq3cUbaQJdBT9Tdp9A=w240-h480-rw" />
+                        <p className=" text-center text-black font-mono ">2500</p>
+                    </div>
+                     <p className=" text-ml font-mono">enterprise organization sucessfully launched</p>
+                </div>
+           </div>
+        </>
+    )
+}
+
+export default Footer;
